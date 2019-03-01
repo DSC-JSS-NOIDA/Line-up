835abc8f7f72450e89d2e46807a41e2d9857e555
tronku <shubham.pathak2000@gmail.com>
2019-03-01 15:39:52 +0530

    Add QR Code generate activity

--- app/src/main/java/project/tronku/line_up/QRCodeActivity.java
+++ app/src/main/java/project/tronku/line_up/QRCodeActivity.java
@@ -2,13 +2,37 @@ package project.tronku.line_up;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class QRCodeActivity extends AppCompatActivity {
+
+    private ImageView myQRCode;
+    private String zealid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+
+        myQRCode = findViewById(R.id.my_qr);
+        zealid = "ZO_2R414";
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(zealid, BarcodeFormat.QR_CODE,220,220);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            myQRCode.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/project/tronku/line_up/login/LoginFragment.java
+++ app/src/main/java/project/tronku/line_up/login/LoginFragment.java
@@ -14,6 +14,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
 import project.tronku.line_up.InstructionsActivity;
+import project.tronku.line_up.QRCodeActivity;
 import project.tronku.line_up.R;
 
 import androidx.fragment.app.Fragment;
@@ -59,8 +60,12 @@ public class LoginFragment extends Fragment implements project.tronku.line_up.lo
         }
         else {
             if (firstTime) {
-                Intent intent = new Intent(getActivity(), InstructionsActivity.class);
-                startActivityForResult(intent, REQUEST_CODE_INTRO);
+                Intent intro = new Intent(getActivity(), InstructionsActivity.class);
+                startActivityForResult(intro, REQUEST_CODE_INTRO);
+            }
+            else {
+                Intent qrcode = new Intent(getActivity(), QRCodeActivity.class);
+                startActivity(qrcode);
             }
         }
     }
