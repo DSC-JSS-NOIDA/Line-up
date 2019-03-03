9090f1bec495c9870d6e706a400d945990a26aaa
atishaya11 <atishaya.11@live.com>
2019-03-04 02:19:01 +0530

    User Info API

--- app/src/main/java/project/tronku/line_up/YourRouteActivity.java
+++ app/src/main/java/project/tronku/line_up/YourRouteActivity.java
@@ -1,23 +1,46 @@
 package project.tronku.line_up;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 public class YourRouteActivity extends AppCompatActivity {
 
+    private static final String TAG = "YourRouteActivity";
     private View layer;
     private ProgressBar loader;
     private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView recyclerView;
     private NetworkReceiver receiver;
+
+    private List<PlayerPOJO> teammatesFound = new ArrayList<>();
+
 
 
     @Override
@@ -52,8 +75,55 @@ public class YourRouteActivity extends AppCompatActivity {
         loader.setVisibility(View.VISIBLE);
         swipeRefreshLayout.setEnabled(false);
 
+        String accessToken = LineUpApplication.getInstance().getAccessToken();
+        if(accessToken == null){
+            Intent mainActivity = new Intent(this, MainActivity.class);
+            LineUpApplication.getInstance().getDefaultSharedPreferences().edit().clear().apply();
+            finishAffinity();
+            startActivity(mainActivity);
+        } else{
+            if (receiver.isConnected()) {
+                Helper.fetchUserInfo(accessToken, new VolleyCallback(){
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onSuccess(String response) {
+                        if(response != null){
+                            teammatesFound = new ArrayList<>(Helper.getPlayersFromResponse(response));
+                            recyclerView.setLayoutManager(new LinearLayoutManager(YourRouteActivity.this));
+                            //recyclerView.setAdapter(adapter);
+                        } else{
+                            Toast.makeText(getApplicationContext(), "Error fetching data, Please try again.", Toast.LENGTH_SHORT).show();
+                        }
+                        layer.setVisibility(View.INVISIBLE);
+                        loader.setVisibility(View.INVISIBLE);
+                        swipeRefreshLayout.setRefreshing(false);
+                        swipeRefreshLayout.setEnabled(true);
+                    }
+
+                    @Override
+                    public void onError(int status, String error) {
+                        if(status == HttpStatus.UNAUTHORIZED.value()){
+                            Toast.makeText(getApplicationContext(), "Please login to perform this action.", Toast.LENGTH_SHORT).show();
+                            LineUpApplication.getInstance().getDefaultSharedPreferences().edit().clear().apply();
+                            Intent login = new Intent(getApplicationContext(), MainActivity.class);
+                            finishAffinity();
+                            startActivity(login);
+                        } else{
+                            Toast.makeText(getApplicationContext(), "Error fetching data, Please try again.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "No internet!", Toast.LENGTH_SHORT).show();
+                layer.setVisibility(View.INVISIBLE);
+                loader.setVisibility(View.INVISIBLE);
+            }
+        }
 
     }
+
+
 
     @Override
     protected void onStart() {
