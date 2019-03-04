e48ab86da7dfce104f548137d59bb387c76012a7
Dheeraj Kotwani <dheeraj.kotwani41@gmail.com>
2019-03-04 22:10:14 +0530

    Add Pokeball

--- app/src/main/java/project/tronku/line_up/QRCodeActivity.java
+++ app/src/main/java/project/tronku/line_up/QRCodeActivity.java
@@ -11,6 +11,7 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 import project.tronku.line_up.timer.CountDownTimerActivity;
 
 import android.Manifest;
+import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
@@ -28,6 +29,9 @@ import android.preference.PreferenceManager;
 import android.provider.SyncStateContract;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -83,7 +87,7 @@ public class QRCodeActivity extends AppCompatActivity {
     private ProgressBar loader;
     private LocationRequest locationRequest;
     private boolean needPermission = false;
-
+    private ImageView poke_top,poke_down;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,6 +102,8 @@ public class QRCodeActivity extends AppCompatActivity {
         logout = findViewById(R.id.logout);
         leaderboard = findViewById(R.id.leaderboard);
         loader = findViewById(R.id.loader_qr);
+        poke_top = findViewById(R.id.pokeball_top);
+        poke_down = findViewById(R.id.pokeball_bottom);
 
         pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
@@ -105,6 +111,44 @@ public class QRCodeActivity extends AppCompatActivity {
         askPermission();
         getLatestLocation();
         startLocationService();
+
+
+        ObjectAnimator animation_y1=ObjectAnimator.ofFloat(poke_top,"translationY",0f, -1500f);
+        animation_y1.setStartDelay(500);
+        animation_y1.setInterpolator(new AccelerateInterpolator());
+        animation_y1.setDuration(1500);
+        animation_y1.start();
+        ObjectAnimator animation_y2=ObjectAnimator.ofFloat(poke_down,"translationY",0, 1500f);
+        animation_y1.setStartDelay(500);
+        animation_y2.setInterpolator(new AccelerateInterpolator());
+        animation_y2.setDuration(1500);
+        animation_y2.start();
+
+        AlphaAnimation back=new AlphaAnimation(1.0f,0.0f);
+        back.setStartTime(500);
+        back.setDuration(2000);
+        back.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                findViewById(R.id.black_background).setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                findViewById(R.id.black_background).setVisibility(View.INVISIBLE);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        findViewById(R.id.black_background).startAnimation(back);
+
+
+
 
         leaderboard.setOnClickListener(new View.OnClickListener() {
             @Override
